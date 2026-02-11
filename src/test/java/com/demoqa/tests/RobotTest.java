f0511ce75a2995d860555fa4fc54ca6c3417cbfd
Oleh Hanziienko <ohanziienko@hotmail.com>
2026-02-11 18:50:01 +0100

    Add GitHub Actions

--- src/test/java/com/demoqa/tests/RobotTest.java
+++ src/test/java/com/demoqa/tests/RobotTest.java
@@ -2,18 +2,18 @@ package com.demoqa.tests;
 
 import com.demoqa.core.TestBase;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.DisabledIfEnvironmentVariable; // Импортируем аннотацию
 import java.awt.Robot;
 import java.awt.event.KeyEvent;
 
 public class RobotTest extends TestBase {
 
     @Test
+    // ЧИТАТЬ ТАК: "Отключи этот тест, если переменная GITHUB_ACTIONS равна true"
+    @DisabledIfEnvironmentVariable(named = "GITHUB_ACTIONS", matches = "true")
     public void robotScrollTest() throws Exception {
-
         Thread.sleep(2000);
-
         Robot robot = new Robot();
-
 
         System.out.println("Робот начинает скроллинг...");
         robot.keyPress(KeyEvent.VK_PAGE_DOWN);
